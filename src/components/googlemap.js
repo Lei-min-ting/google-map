5f857255a2592b0927677522dfd1656aa4f37b96
Minting Lei <lmt19950609@gmail.com>
2021-04-27 16:47:54 -0400

    second adding

--- src/components/googlemap.js
+++ src/components/googlemap.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import { Map, GoogleApiWrapper, Marker,infoWindow } from "google-maps-react";
 import Card from "./inforCard";
 
 const mapStyles = {
@@ -49,8 +49,15 @@ const GroupBuyLeaerInfo = [
 export const MapContainer = (props) => {
   const [groupBuyleaderId, setGroupBuyleaderId] = useState("");
   const [filterLeaderInfo, setFilterLeaderInfo] = useState({});
+  const [currentLocation,setCurrentLocation] = useState({});
+  const [showCurrentLocaMark,setShowCurrentLocaMark] = useState(false);
+  const [zoomsize,setzoom] = useState(3);
+
   const clickRef = useRef(groupBuyleaderId);
         clickRef.current = groupBuyleaderId;
+  
+  const currentRef = useRef(currentLocation);
+        currentRef.current = currentLocation;
 
   const GroupLeaderInfoSendHandler = (id) => {
     setGroupBuyleaderId(id);
@@ -61,19 +68,49 @@ export const MapContainer = (props) => {
     }
   };
 
+  let marker=null;
+  const CurrentLocationIdentifier =()=>{
+    console.log('1');
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          console.log('pos:'+pos)
+          setCurrentLocation(pos)
+        })
+        console.log(currentLocation);
+        Map.setCenter({lat:currentLocation.lat,lng:currentLocation.lng});
+        setzoom(6);
+         //marker = <Marker position ={currentLocation}/> 
+         console.log('Marker:'+ marker);
+        //setShowCurrentLocaMark(true);
+          /*infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);*/
+        }
+  }
+  
+
 
   return (
     <div className="grid grid-cols-2 bg-gray-200 h-screen">
+      
       <div className="pt-20 pl-10 ">
+      <button onClick={CurrentLocationIdentifier}>Pan to current Location</button>
         <Map
           google={props.google}
-          zoom={3}
+          zoom={zoomsize}
           style={mapStyles}
           initialCenter={{
             lat: 38.51417683162813,
             lng: -99.32401849641076,
           }}
         >
+          <Marker position={currentLocation}/>
           {loca.map((location) => {
             return (
               <Marker
